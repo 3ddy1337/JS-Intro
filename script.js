a078b0d2e20e046c4d725484b5e02256e208ebd3
3ddy1337 <em@2mrk.de>
2024-03-21 10:53:46 +0100

    add tasks

--- script.js
+++ script.js
@@ -178,6 +178,8 @@ function oddNumbers(num1, num2) {
     return "Invalid parameters. Only positive integers are allowed.";
   }
 
+  // i = index
+
   let result = "";
   for (let i = num1; i <= num2; i++) {
     if (i % 2 !== 0) {
@@ -218,3 +220,73 @@ function charCount(word, char) {
 console.log(charCount("hello", "l")); // result should be: 2
 console.log(charCount("mama", "m")); // result should be: 2
 console.log(charCount("Resümee", "e")); // result should be: 3
+
+// removeItem function ***************************************************************
+
+/*
+Implement a removeItem function.
+
+ The first parameter is an array.
+ The second parameter is a number. It's the non-zero-based index of the item that should get deleted.
+ Remove the item and return an array.
+ Ensure that the original array was not mutated.
+ */
+
+// TODO: Implement the removeItem function
+
+function removeItem(arr, index) {
+  const newArray = arr.slice();
+  newArray.splice(index, 1);
+  return newArray;
+}
+
+const ainmals = ["Dog", "Cat", "Lion"];
+console.log(removeItem(ainmals, 0));
+// result should be: ["Cat", "Lion"]
+
+console.log(ainmals);
+// result should be still: ["Dog", "Cat", "Lion"]
+
+const fruits = ["Watermelon", "Banana", "Cherry", "Kiwi", "Pineapple", "Apple"];
+console.log(removeItem(fruits, 2));
+// result should be: ["Watermelon", "Banana", "Kiwi", "Pineapple", "Apple"]
+
+console.log(fruits);
+// result should be still: ["Watermelon", "Banana", "Cherry", "Kiwi", "Pineapple", "Apple"]
+
+// sumOfCharacter function ************************************************************
+
+/*
+Implement a sumOfCharacters function.
+
+ The function has one parameter, which is an array.
+ Check the type of each array entry. If it's a string then store the count of characters.
+ The function returns the total sum of all characters.
+// TODO: Implement the sumOfCharacters function
+*/
+
+function sumOfCharacters(arr) {
+  let sum = 0;
+
+  for (let item of arr) {
+    if (typeof item === "string") {
+      sum += item.length;
+    }
+  }
+
+  return sum;
+}
+
+const arr1 = ["Luke", "Anakin", true, "Obi Wan", 333];
+console.log(sumOfCharacters(arr1));
+// result should be: 17
+
+const arr2 = [
+  "Code is",
+  "like humor",
+  ".",
+  "When you have",
+  "to explain it, it's bad!",
+];
+console.log(sumOfCharacters(arr2));
+// result should be: 55
